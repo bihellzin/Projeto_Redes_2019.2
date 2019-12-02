fe94500f1dcadfca9d258bdd5e7f58decb225569
André Filho <50752448+mrdedede@users.noreply.github.com>
2019-12-02 17:16:06 -0300

    Adaptação do cliente
    Agora ele tem uma comunicação já bem definida com o tracker em relação aos procedimentos, mas ainda falta adaptá-lo para um funcionamento eficaz em alguns métodos, bem como falta fazer alguns deles do zero.

--- client.py
+++ client.py
@@ -1,52 +1,95 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Wed Nov 27 16:31:12 2019
+Last Modified: 02/12/2019
 
 @author: vrs2
+
+@author2: acasf
 """
 import socket
-import sys
-#Estabelecendo uma conexão com o Tracker
-#O IP do server está representado por "x"
-connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('x', 777)
-#Função usada para controlar envio e recepção de requerimentos pelo Tracker
-def controlpanel():
-    flag=True
-    while flag == True:
-        command=int(input("Deseja requisitar algum arquivo(1) ou sair do sistema(2)?"))
-        if command == 1:
-            request()
-        if command == 2:
-            flag = False
-    command=int(input("Deseja fechar a conexão? Se sim digite (1). Caso não,para abrir novamente o painel de controle (2)"))
-    if command == 1:
-        connect.close()
-    else:
-        controlpanel()
-def request():
-    archive=input("Digite o nome do arquivo")
-    connect.sendall(archive)
-    received=0
-    expected=len("positive")
-    while received < expected:
-        data = connect.recv(16)
-        received += len(data)
-    if len(received) == len(expected):
-        address=()
-        size=30
-        while len(address) < size:
-            data = connect.recv(15)
-            address += len(data)
-        getarchive(address)
-    else:
-        print("O arquivo não está disponível :(")
-def getarchive(address):
-    connect.sendall("getsize")
-    
-    
-def sendarchive(address):
-    
+#Classe obrigatória para o trabalho
 
+import os
+#Essa classe nos vai auxiliar a identificar o tamanho dos arquivos
+#Bem como criará a pasta do compartilhamento se necessário 
+
+
+class Client:
+    def __init__(self):
+        self.appear_on = True
+
+    def new_stuff(self):
+        #Incomplete
+        #I also don't knwo how to get our own IP address
+        if(os.path.exists("share")):
+            os.mkdir("share")
+        else:
+            pass
+        #Same questions as "im_off()", might ask ehammo
+        
     
+    def send_request(self,moi, what):
+        server_address = ('localhost', 777)
+        moi.connect(server_address)
+        try:
+            data = "REQ" + what
+            moi.sendall(data)
+            who = moi.recv(16)
+        except:
+            moi.close()
+        return who
+                
+    def im_off(self, moi):
+        #IDK how to procceed here. Might ask ehammo
+        server_address = ('localhost', 777)
+        moi.connect(server_address)
+        try:
+            msg = "SOM"
+            moi.sendall(msg)
+            #IDK how to get our own IP
+            data = moi.recv(2)
+        finally:
+            moi.close()
+        self.appear_on = False
+        return data
+    
+    def its_off(self ,moi, peer):
+        server_address = ('localhost', 777)
+        moi.connect(server_address)
+        try:
+            msg = "SOM" + peer
+            moi.sendall(msg)
+            data = moi.recv(2)
+            #The length of the response will be only 2 chars
+        finally:
+            moi.close()
+        return data
+    
+    def peer_lost(self, who, what):
+        pass
+        #This will tell the tracker when a peer lost some data
+        #The datatype code is "LOS"
+    
+    def main(self):
+        connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #Still needs improvements
+        flag=True
+        while flag == True:
+            command=int(input("Deseja requisitar algum arquivo(1) ou sair do sistema(2)?"))
+            #We shouldn't use inputs in classes
+            if command == 1:
+                self.send_request(connect)
+            if command == 2:
+                #Change it to an elif
+                flag = False
+                #There are more than 3 options in this scenario
+        command=int(input("Deseja fechar a conexão? Se sim digite (1). Caso não,para abrir novamente o painel de controle (2)"))
+        #We shouldn't use inputs in classes
+        if command == 1:
+            connect.close()
+        else:
+            self.main()
+            #I'm skeptical about the functionability of this
+    
+    def getarchive(self,connect, address):
+        #Incomplete
+        connect.sendall("getsize")
