92ae580d7f7bff2345a6dc255b271722b5ff73c8
vrs2 <51673690+vrs2@users.noreply.github.com>
2019-12-02 16:42:38 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ def controlpanel():
     while flag == True:
         command=int(input("Deseja requisitar algum arquivo(1) ou sair do sistema(2)?"))
         if command == 1:
-            connect.sendall("1")
+            request()
         if command == 2:
             flag = False
     command=int(input("Deseja fechar a conexão? Se sim digite (1). Caso não,para abrir novamente o painel de controle (2)"))
@@ -25,4 +25,28 @@ def controlpanel():
         connect.close()
     else:
         controlpanel()
+def request():
+    archive=input("Digite o nome do arquivo")
+    connect.sendall(archive)
+    received=0
+    expected=len("positive")
+    while received < expected:
+        data = connect.recv(16)
+        received += len(data)
+    if len(received) == len(expected):
+        address=()
+        size=30
+        while len(address) < size:
+            data = connect.recv(15)
+            address += len(data)
+        getarchive(address)
+    else:
+        print("O arquivo não está disponível :(")
+def getarchive(address):
+    connect.sendall("getsize")
+    
+    
+def sendarchive(address):
+    
+
     
